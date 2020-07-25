3a3767e48f70ba6a8e5273bb67a60290a0c92dc9
MatteOS88 <66838853+MatteOS88@users.noreply.github.com>
2020-07-25 10:33:39 +0200

    update

--- translator/translator.js
+++ translator/translator.js
@@ -1,5 +1,5 @@
-const translateWord = ['embed', 'plumber', 'velocity', 'disgrace', 'barely', 'capable', 'awe', 'withdraw', 'former', 'insult', 'stubborn', 'semicolon', 'inevitable', 'plain', 'otherwise', 'well', 'bank', 'wipe', 'overwhelming'];
-const polishWord = ['osadzać', 'hydraulik', 'prędkość', 'hańba', 'ledwo', 'zdolny', 'groza', 'wycofać', 'były', 'zniewaga', 'uparty', 'średnik', 'nieunikniony', 'równina', 'inaczej', 'studnia', 'brzeg', 'wycierać', 'przytłaczający'];
+const translateWord = ['embed', 'plumber', 'velocity', 'disgrace', 'barely', 'capable', 'awe', 'withdraw', 'former', 'insult', 'stubborn', 'semicolon', 'inevitable', 'plain', 'otherwise', 'well', 'bank', 'wipe', 'overwhelming', 'contribution', 'perplexing'];
+const polishWord = ['osadzać', 'hydraulik', 'prędkość', 'hańba', 'ledwo', 'zdolny', 'groza', 'wycofać', 'były', 'zniewaga', 'uparty', 'średnik', 'nieunikniony', 'równina', 'inaczej', 'studnia', 'brzeg', 'wycierać', 'przytłaczający', 'wkład', 'kłopotliwy'];
 
 const sectionFirst = document.querySelector('section.first')
 const sectionCounter = document.querySelector('section.counter')
